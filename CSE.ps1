7bfcf3f763af2d67dfea4fe46638aa4e066b0311
Victor Santana <vsantana@microsoft.com>
2018-09-03 22:30:35 -0500

    Fix download Powershell. Disable IE Sec Extension. Using Fixed Priv. IP.

--- CSE.ps1
+++ CSE.ps1
@@ -22,7 +22,7 @@ for ($i = 1; $i -lt $UsersAmount+1; $i++) {
     Add-DnsServerResourceRecordA -Name "*" -ZoneName "cssilbaseuser$i.com" -IPv4Address "10.11.$i.11" -TimeToLive 00:00:00
     Add-DnsServerResourceRecordA -Name "*.scm" -ZoneName "cssilbaseuser$i.com" -IPv4Address "10.11.$i.11" -TimeToLive 00:00:00
 
-    New-LocalUser -Name "User$i" -Password (ConvertTo-SecureString -AsPlainText -String "No_P@ssw0rd" -force) -AccountNeverExpires:$True -PasswordNeverExpires:$True -UserMayNotChangePassword:$True
+    New-LocalUser -Name "User$i" -Password (ConvertTo-SecureString -AsPlainText -String "No_P@ssw0rd!" -force) -AccountNeverExpires:$True -PasswordNeverExpires:$True -UserMayNotChangePassword:$True
     Add-LocalGroupMember -Group Administrators -Member "User$i"
 }
 
@@ -30,3 +30,13 @@ for ($i = 1; $i -lt $UsersAmount+1; $i++) {
 # Get-DnsServerZone  | where{$_.ZoneName -like "css*"} | Remove-DnsServerZone -Force
 # Get-LocalUser user* | Remove-LocalUser
 
+function Disable-InternetExplorerESC {
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+    #Stop-Process -Name Explorer -force
+    #Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+}
+
+Disable-InternetExplorerESC
